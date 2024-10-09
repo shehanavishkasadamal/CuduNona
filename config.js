486275155e668a2fa7d3efbcaee15521c603f261
Epzi <99525582+HaCkr-EPZI-public@users.noreply.github.com>
2024-10-09 19:50:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "7d8FmS7I#Lnt4Lz46fVugIM78Yu382EzeZ6dep3l5H4ZQ3yof3Ko",
+SESSION_ID: process.env.SESSION_ID || "",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
