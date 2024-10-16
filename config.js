c47b6aeb3ab4b99a523e2271ef3dd76bbaa9757f
shehanavishkasadamal <126753337+shehanavishkasadamal@users.noreply.github.com>
2024-10-16 11:19:25 +0530

    config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID:AyFHAITZ#sfVRXW0UBmcUuHZ_rLSv3r5Ge2x_uwAjYr7RkyucN3k process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
